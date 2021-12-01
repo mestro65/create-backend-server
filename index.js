4a8eb2ffb27eb9aca78789231be712bc7d3d2027
Marko Mestrovic <67430555+mestro65@users.noreply.github.com>
2021-12-01 17:20:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = function createServer() {
     const express = require('express')
-    const port = 3000 | 3010 | 5000
+    const port = process.env.PORT || 3000 || 3010 || 5000
     const app = express()
     const http = require('http').Server(app)
 
-    http.listen(port, () => {
-        console.log("Server works at port %d", port, "http://localhost:"+ port)
+    const server = http.listen(port, () => {
+        console.log("Server works at port %d", port, "http://localhost:" + port)
     })
 }
